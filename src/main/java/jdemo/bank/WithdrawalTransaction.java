544b13cce7f914bddb93309f1e94f38c93fff07c
Maciej Glowa <maciej.glowa@parasoft.com>
2022-10-13 15:33:11 +0200

    Fix formatting in WithdrawalTransaction class.

--- src/main/java/jdemo/bank/WithdrawalTransaction.java
+++ src/main/java/jdemo/bank/WithdrawalTransaction.java
@@ -5,17 +5,20 @@ package jdemo.bank;
  * 
  * @author John
  */
-public class WithdrawalTransaction extends AbstractTransaction {
-
+public class WithdrawalTransaction
+    extends AbstractTransaction
+{
     private static final int MAX_WITHDRAW = 1000;
 
     public WithdrawalTransaction(int amount) {
-        if ((amount < 0) && (amount > MAX_WITHDRAW))
+        if ((amount < 0) && (amount > MAX_WITHDRAW)) {
             throw new IllegalArgumentException("Invalid withdrawal amount: " + amount);
+        }
         _amount = amount;
     }
 
-    public boolean apply(Account account) {
+    public boolean apply(Account account)
+    {
         account.setBalance(account.getBalance() - _amount);
         return true;
     }
